78ec9268753bb2925b1e2e73c6120eaaa268bc73
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-02-28 08:43:39 -0300

    feat: instalação das dependencias boostrap-vue, bootstrap, vue, vue-template-compiler e core-js. Todos necessários estar nas exatas versões para funcionar o 'boostrap-vue'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import Vue from 'vue';
+import App from './App.vue';
+
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin)
+
 
 new Vue({
   render: h => h(App),
